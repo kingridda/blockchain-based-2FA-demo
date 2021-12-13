66e001f6ef7e97b1574fd115b3354843b67a82cd
kingridda <abdelghaniridda01@gmail.com>
2021-12-13 03:33:11 +0100

    express works

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 var express = require('express');
+var app = express();
+var path = require('path');
+const port = 3000
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/registration.html');
+});
+
+app.get('/verification', function(req, res) {
+    res.sendFile(__dirname + '/public/authCode.html');
+});
+app.get('/home', function(req, res) {
+    res.sendFile(__dirname + '/public/welcomePage.html');
+})
+
+
+
+
+
+
+
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res) {
+    res.status(404).send('404');
+});
+// error handler
+app.use(function(err, req, res, next) {
+    // render the error page
+    res.status(500).send('ERROR 500');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
